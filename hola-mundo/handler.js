0d7dd91b5e7cf462a1324d7d00b8edf86e8244fb
jhc <jhernandez01@hotmail.com>
2023-04-14 23:57:04 +0200

    first commit

--- hola-mundo/handler.js
+++ hola-mundo/handler.js
@@ -1,11 +1,14 @@
 'use strict';
+//create file package.json
+//npm init -y
+
 
 module.exports.hello = async (event) => {
   return {
     statusCode: 200,
     body: JSON.stringify(
       {
-        message: 'Go Serverless v1.0! Your function executed successfully!',
+        message: 'Go Serverless v1.0! Your function executed successfully!!',
         input: event,
       },
       null,
